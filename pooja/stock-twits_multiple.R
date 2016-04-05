f010bf704ff446532ae009aec74d4286f2c81099
goodwillyoga <goodwillyoga@gmail.com>
2016-04-04 22:13:50 -0400

    Updated - writing records to cvs before every sleep

--- pooja/stock-twits_multiple.R
+++ pooja/stock-twits_multiple.R
@@ -5,7 +5,7 @@ msgs.df <- c()
 trending_stock <- c("GILD","EIX","GS","AMZN", "RKUS","AAPL","POM","GRPN","XIV","YHOO","VA","MSFT","TSLA","BSX","NVDA","ORCL","EW","CPGX","MRK","V","BXLT","FOXA","ERIC","AVP","TWX","CMCSA","XRX","WY","GNCA","WBA","MO","MA","FOLD","TLT","SNY","RTN","UTX","LOW","MAS","GPT","RICE","IBM","KHC","CDNS","ANTM","HD","INO","OCLR","LULU","SABR","DYN","AXLL","WEN","COH","GOOG","FB","TWTR")
 
 
-for(k in c(1:48))
+for(k in c(1:50))
 {
   stock_twits <- function(msgs.df, stock){
     #Get raw data
@@ -27,10 +27,11 @@ for(k in c(1:48))
 
   stock_twits(msgs.df, trending_stock)
   print(paste("ite", k))
+  write.csv(msgs.df, file = "/Users/poojasingh/Documents//HE107/stock-twits-multiple.csv", row.names=FALSE, col.names=FALSE, append=TRUE)
   Sys.sleep(15*60) #sleep for 15 minutes
 }
 
 View(msgs.df)
 
-write.csv(msgs.df, file = "/Users/poojasingh/Documents//HE107/stock-twits-multiple.csv", row.names=FALSE, col.names=FALSE, append=TRUE)
+#write.csv(msgs.df, file = "/Users/poojasingh/Documents//HE107/stock-twits-multiple.csv", row.names=FALSE, col.names=FALSE, append=TRUE)
 
