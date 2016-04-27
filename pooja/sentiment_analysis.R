d06dcff1e7c8a3294e929b1cbc2b9e65b53c2e2a
goodwillyoga <goodwillyoga@gmail.com>
2016-04-27 16:12:12 -0400

    Added join

--- pooja/sentiment_analysis.R
+++ pooja/sentiment_analysis.R
@@ -10,17 +10,18 @@ colnames(data)[3] <- "createdat"
 #Convert to UTC to EST time zone 
 data <- data %>% mutate(utc.time = as.POSIXct(data$createdat, tz="UTC")) %>%
                  mutate(est.time = format(utc.time, tz="EST")) %>%
-                 mutate(est.date = as.Date(est.time))
+                 mutate(est.date = substr(as.character(format(strftime(est.time, '%m/%d/%Y'))), 2,10))
+#head(data$est.date)
 
 #Average the sentiment score by day
-avg_sentiment_score <- data %>% group_by(symbol, est.date) %>%
+avg_sentiment_score <- data %>% group_by(symbol, day = est.date) %>%
             mutate(avg_score = mean(sentiment_score)) %>%
             select(symbol, est.date, avg_score) %>%
             unique() %>% 
             arrange(symbol, est.date)
   
-View(data)
-View(avg_sentiment_score)
+#View(data)
+#View(avg_sentiment_score)
 
 #Bar plot for high-volatility and low-volatility??
 data %>% filter(symbol %in% c("APPL", "YHOO", "MSFT", "TSLA", "GOOG", "FB", "EIX", "GS")) %>%
@@ -33,11 +34,14 @@ avg_sentiment_score %>% filter(symbol %in% c("APPL", "YHOO", "MSFT", "TSLA", "GO
 
 
 load("/Users/poojasingh/Documents/HE107/E107project/pulkit/yahoo-finance.RData")
-View(stocks)
+#View(stocks)
 avg_stocks_price <- stocks %>% 
                    group_by(symbol, day = lastTradeDate) %>%
                    mutate(avg_price = mean(price)) %>%
                    select(symbol, day, avg_price )
-View(avg_stocks_price)
+head(avg_stocks_price$day)
 
+#Plot stock Price Vs. by day and sentiment score as the size
+dat <- inner_join(avg_sentiment_score, avg_stocks_price)
+View(dat)
 
