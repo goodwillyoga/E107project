05f860dae80b1f2c1dab6700f01e67bb04481a75
goodwillyoga <goodwillyoga@gmail.com>
2016-04-28 00:10:51 -0400

    Added files via upload

--- pooja/sentiment_analysis.R
+++ pooja/sentiment_analysis.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(readr)
 library(stringr)
 library(ggplot2)
+library(lubridate)
 
 #read stick-twits_sentiments generate from stock_twits_processing.R file
 data <- read_csv(file = "https://raw.githubusercontent.com/goodwillyoga/E107project/master/pooja/data/stock_twits_sentiment_score.csv")[-1]
@@ -9,19 +10,21 @@ colnames(data)[3] <- "createdat"
 
 #Convert to UTC to EST time zone 
 data <- data %>% mutate(utc.time = as.POSIXct(data$createdat, tz="UTC")) %>%
-                 mutate(est.time = format(utc.time, tz="EST")) %>%
-                 mutate(est.date = substr(as.character(format(strftime(est.time, '%m/%d/%Y'))), 2,10))
-#head(data$est.date)
+                 mutate(est.time = format(utc.time, tz="America/New_York")) %>%
+                 mutate(est.date = substr(as.character(format(strftime(est.time, '%m/%d/%Y'))), 2,10)) %>%
+                 mutate(est.hour = paste(est.date, hour(est.time)))
+#head(data$utc.time)
+#head(data$est.time)
+#head(data$est.hour)
 
 #Average the sentiment score by day
-avg_sentiment_score <- data %>% group_by(symbol, day = est.date) %>%
+avg_sentiment_score <- data %>% group_by(symbol, dayhour = est.hour) %>%
             mutate(avg_score = mean(sentiment_score)) %>%
-            select(symbol, est.date, avg_score) %>%
-            unique() %>% 
-            arrange(symbol, est.date)
+            select(symbol, dayhour, avg_score) %>%
+            unique() 
   
 #View(data)
-#View(avg_sentiment_score)
+#View(avg_sentiment_score$dayhour)
 
 #Bar plot for high-volatility and low-volatility??
 data %>% filter(symbol %in% c("APPL", "YHOO", "MSFT", "TSLA", "GOOG", "FB", "EIX", "GS")) %>%
@@ -33,23 +36,37 @@ avg_sentiment_score %>% filter(symbol %in% c("APPL", "YHOO", "MSFT", "TSLA", "GO
   geom_histogram() 
 
 
+#get hour from time
+get_24hour <- function(x){
+  ret <- ''
+  splitVector<- strsplit(x,':')
+  ret <- sapply(splitVector, function(z){
+    if(str_count(z[2],'AM')|str_count(z[2],'am')){
+      paste( z[1])
+    }else{
+      paste( ifelse(as.numeric(z[1]) == 12,12, as.numeric(z[1])+12 ))
+    }
+  })
+  return(ret)
+}
+
 load("/Users/poojasingh/Documents/HE107/E107project/pulkit/yahoo-finance.RData")
 #View(stocks)
 avg_stocks_price <- stocks %>% 
-                   group_by(symbol, day = lastTradeDate) %>%
-                   mutate(avg_price = mean(price)) %>%
-                   select(symbol, day, avg_price )
-head(avg_stocks_price$day)
+                   mutate(dayhour = paste(lastTradeDate,get_24hour(lastTradeTime))) %>%
+                   group_by(symbol, dayhour) %>%
+                   mutate(close_price = mean(price)) %>%
+                   select(symbol, dayhour, close_price) %>%
+                   unique()
+#View(avg_stocks_price$dayhour )
 
 #Plot stock Price Vs. by day and sentiment score as the size
 dat <- inner_join(avg_sentiment_score, avg_stocks_price)
-View(dat)
-
-#dat %>% filter(symbol %in% c("AAPL", "YHOO", "MSFT", "TSLA", "GOOG", "FB", "EIX", "GS")) %>%
-#  ggplot(aes(x = day, y=avg_score, size=avg_score, color=symbol)) + geom_point()
+#View(dat)
 
 dat %>% filter(symbol %in% c("AAPL", "EIX", "GS")) %>%
-  ggplot(aes(x = day, y=avg_price, size=avg_score, color=symbol, label=symbol)) + geom_point() +
-  labs(title="Daily Stock Price and Sentiment Score", x="Day of the week", y="Average Stock Price of the day")
+  ggplot(aes(x = dayhour, y=close_price, size=avg_score, color=symbol, label=symbol)) + geom_point() +
+  labs(title="Hourly Stock Price and Sentiment Score", x="Hour of the Day", y="Stock Price") +
+  theme(axis.text.x = element_blank())
 
 
